8d171eb15b108f10e07d006e2c9e4b6cbb0a0147
liwen <liwen111111>
2021-03-15 22:46:28 +0800

    手写promise

---
+++ promise/promise.js
@@ -0,0 +1,160 @@
+const PENDING = 'pending'
+const FULFILLED = 'fulfilled'
+const REJECTED = 'rejected'
+
+
+function resolvePromise(promise2, x, resolve, reject) {
+  if (promise2 === x) {
+    return reject(new TypeError('Chaining cycle detected for promise'))
+  }
+  if (typeof x === 'object' && x !== null || typeof x === 'function') {
+    try {
+      let then = x.then
+      if (typeof then === 'function') {
+        // 表示他是一个promise
+        then.call(x, y => { // 成功回调
+          resolvePromise(promise2, y, resolve, reject)
+        }, z => { // 失败回调
+          reject(z)
+        })
+      } else {
+        // 表示他是一个单纯的对象 obj = {then: '1111'}
+        resolve(x)
+      }
+    } catch (error) {
+      reject(err)
+    }
+  } else {
+    resolve(x)
+  }
+}
+
+class Promise {
+  constructor(executor) { // 类的构造函数，this指向实例，constructor的属性会被实例复制变成实例对象上的自身属性
+    this.state = PENDING
+    this.value = undefined
+    this.reason = undefined
+    this.onResolvedCbs = []
+    this.onRejectedCbs = []
+    let resolve = (value) => {
+      this.value = value
+      this.state = FULFILLED
+      this.onResolvedCbs.forEach(cb => cb(value))
+    }
+    let reject = (reason) => {
+      this.reason = reason
+      this.state = REJECTED
+      this.onRejectedCbs.forEach(cb => cb(reason))
+    }
+    try {
+      executor(resolve, reject)
+    } catch (error) {
+      reject(error)
+    }
+  }
+
+  then(onFulfilled, onRejected) {
+
+    onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : value => value
+    onRejected = typeof onRejected === 'function' ? onRejected : err => { throw err }
+
+    let promise2 = new Promise((resolve, reject) => {
+      // 用node微任务更加贴合promise是作为微任务执行
+      // 在promise回调是异步的时候(如： setTimeout 才能拿到setTimeout resolve的值， 并且才能拿到声明的promise2)
+      process.nextTick(() => { 
+        if (this.state === FULFILLED) {
+          try {
+            let x = onFulfilled(this.value)
+            resolvePromise(promise2, x, resolve, reject)
+          } catch (error) {
+            reject(error)
+          }
+        }
+        if (this.state === REJECTED) {
+          try {
+            let x = onRejected(this.reason)
+            resolvePromise(promise2, x, resolve, reject)
+          } catch (error) {
+            reject(error)
+          }
+        }
+        if (this.state === PENDING) {
+          this.onResolvedCbs.push(() => {
+            try {
+              let x = onFulfilled(this.value)
+              resolvePromise(promise2, x, resolve, reject)
+            } catch (error) {
+              reject(error)
+            }
+          })
+          this.onRejectedCbs.push(() => {
+            try {
+              let x = onRejected(this.reason)
+              resolvePromise(promise2, x, resolve, reject)
+            } catch (error) {
+              reject(error)
+            }
+          })
+        }
+      }, 0)
+    })
+    return promise2
+  }
+
+  catch(fn) { // 类prototype上的方法，被实例上的_proto_所共享，实例在自身属性中找不到某个属性，就会来类的prototype上找
+    return this.then(null, fn)
+  }
+
+  finally(cb) {
+    debugger
+    let P = this.constructor
+    return this.then(
+      value => P.resolve(cb()).then(() => value),
+      reason => P.resolve(cb()).then(() => { throw reason })
+    )
+  }
+}
+Promise.resolve = function(val) { // 类的私有属性（方法）， 只有类可以调用，实例不能调用。还可以写在类里面，前缀加 static
+  return new Promise(resolve => resolve(val))
+}
+
+Promise.reject = function(val) {
+  return new Promise((resolve, reject) => {reject(val)})
+}
+
+function isPromise(val) {
+  if (typeof val === 'object' && val !== null || typeof val === 'function') {
+    if (typeof val.then === 'function') {
+      return true
+    } else {
+      return false
+    }
+  } else {
+    return false
+  }
+}
+
+Promise.all = function(arr) {
+  let a = []
+  let i = 0
+  function processData(i, data, resolve) {
+    a[i] = data
+    i++
+    if (i === arr.length) {
+      resolve(a)
+    }
+  }
+  return new Promise((resolve, reject) => {
+    for (let i = 0; i < arr.length; i++) {
+      if (isPromise(arr[i])) {
+        arr[i].then(data => {
+          processData(i, data, resolve)
+        }, reject)
+      } else {
+        processData(i, data, resolve)
+      }
+    }
+  })
+}
+
+module.exports = Promise
---
+++ promise/test.js
@@ -0,0 +1,75 @@
+const Promise = require('./promise')
+// const Promise = require('./promise2')
+
+// const promise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     if (false){
+//       resolve('1111');
+//     } else {
+//       reject('错误');
+//     }
+//   }, 0)
+// });
+// setTimeout(() => {
+//   console.log('8888')
+// }, 0)
+// promise.then().then(data => {
+//   debugger
+//   console.log('suc:'+data)
+// }, (err) => {
+//   console.log('err:' +err)
+// })
+
+// promise.catch((err) => {
+//   debugger
+//   console.log('err:' +err)
+//   return '111'
+// }).then(data => {
+//   console.log(data)
+// })
+// let thenable = {
+//   then: function(resolve, reject) {
+//     resolve(42);
+//   }
+// };
+// let thenable = new Promise((resolve, reject) => {
+//   resolve(4)
+// })
+// Promise.resolve(4).then(data => {
+//   console.log(data)
+// })
+// console.log(thenable)
+// promise.reject(22).catch(data => {
+//   console.log(data)
+// })
+
+// class Foo {
+//   static a = 1
+//   b = 2
+//   static classMethod() {
+//    console.log(1111)
+//   }
+// }
+
+
+// Foo.classMethod = function() {
+//   console.log('555')
+// }
+// Foo.a = 1
+
+// var foo = new Foo();
+// Foo.classMethod()
+// console.log(foo.a)
+// console.log(foo.b)
+
+// new Promise((resolve, reject) => {
+//   resolve(222)
+// }).then(data => {
+//   console.log(data)
+// }).finally(() => {
+//   console.log('fff')
+// })
+let arr = [1, 3, 5]
+Promise.all(arr).then(data => {
+  console.log(data)
+})
